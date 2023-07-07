e84f67c801fe08a6f1ff5120fc0fcb9d321c4e51
chichoon <chichoon.choi@gmail.com>
2023-07-07 16:13:04 +0900

    fix: removeDB 변수 삭제

--- src/utils/removeDB.ts
+++ src/utils/removeDB.ts
@@ -4,5 +4,5 @@ export function removeDB() {
     window.alert("해당 브라우저에서는 Indexed DB를 지원하지 않습니다.");
     return; // TODO: throw로 바꾸기
   }
-  const db = window.indexedDB.deleteDatabase("WordDB");
+  window.indexedDB.deleteDatabase("WordDB");
 }
