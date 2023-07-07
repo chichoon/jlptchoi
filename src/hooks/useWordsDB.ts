e1509aa1c80b0dd95c19ace9133d4276da523dca
chichoon <chichoon.choi@gmail.com>
2023-07-07 18:47:36 +0900

    fix: useWordsDB hook 수정

--- src/hooks/useWordsDB.ts
+++ src/hooks/useWordsDB.ts
@@ -1,6 +1,6 @@
 import { Word } from "@/types/Words";
 
-export function useWordsDB() {
+const dbPromise = new Promise<IDBDatabase | null>((resolve, reject) => {
   const indexedDB = window.indexedDB;
   if (!indexedDB) {
     window.alert("해당 브라우저에서는 Indexed DB를 지원하지 않습니다.");
@@ -8,57 +8,78 @@ export function useWordsDB() {
   }
 
   const request = indexedDB.open("WordDB");
-  request.onerror = function (e: Event) {
-    throw Error("database error");
+
+  request.onsuccess = function (e: Event) {
+    resolve(e.target?.result ?? null);
   };
-  const db = request.result;
-
-  function getWordCount() {
-    const result = db
-      .transaction("words", "readwrite")
-      .objectStore("words")
-      .count();
-    result.onsuccess = function (e: Event) {
-      return result;
-    };
-    result.onerror = function (e: Event) {
-      return 0;
-    };
-    return result.result;
-  }
-
-  function getWord(index: number) {
-    const result = db
-      .transaction("words", "readwrite")
-      .objectStore("words")
-      .get(index);
-    result.onsuccess = function (e: Event) {
-      return result;
-    };
-    result.onerror = function (e: Event) {};
-    return result.result;
-  }
-
-  function saveToDB(words: Word[]) {
-    const transaction = db.transaction("words", "readwrite");
-    transaction.onerror = function (e: Event) {
-      console.log(e.target);
-    };
-    const objectStore = transaction.objectStore("words");
-    words.forEach((word) => {
-      let request = objectStore.put(word);
-      request.onsuccess = function (e: Event) {};
+  request.onerror = function (e: Event) {
+    reject(e);
+  };
+  request.onupgradeneeded = function (e: Event) {
+    const db = request.result;
+    const objectStore = db.createObjectStore("words", {
+      autoIncrement: true,
     });
+    objectStore.createIndex("word", "word", { unique: true });
+    objectStore.createIndex("meaning", "meaning", { unique: false });
+    objectStore.createIndex("pronunciation", "pronunciation", {
+      unique: false,
+    });
+  };
+});
+
+export function useWordsDB() {
+  async function getWordCount(): Promise<number> {
+    const db = await dbPromise;
+    if (!db) return 0;
+    try {
+      return db.transaction("words", "readwrite").objectStore("words").count()
+        .result;
+    } catch (_) {
+      return 0;
+    }
   }
 
-  function removeFromDB(index: number) {
-    const transaction = db.transaction("words", "readwrite");
-    transaction.onerror = function (e: Event) {
-      // TODO : error handling
-    };
-    const result = transaction.objectStore("words").delete(index);
-    result.onsuccess = function (e: Event) {};
-    result.onerror = function (e: Event) {};
+  async function getWord(index: number): Promise<Word | null> {
+    const db = await dbPromise;
+    if (!db) return null;
+    try {
+      return db
+        .transaction("words", "readwrite")
+        .objectStore("words")
+        .get(index).result;
+    } catch (_) {
+      return null;
+    }
+  }
+
+  async function saveToDB(words: Word[]): Promise<void> {
+    const db = await dbPromise;
+    if (!db) return;
+    try {
+      const objectStore = db
+        .transaction("words", "readwrite")
+        .objectStore("words");
+      words.forEach((word) => {
+        let request = objectStore.put(word);
+        request.onsuccess = function (e: Event) {};
+      });
+    } catch (_) {
+      return; // TODO: error handling
+    }
+  }
+
+  async function removeFromDB(index: number): Promise<void> {
+    const db = await dbPromise;
+    if (!db) return;
+    try {
+      return db
+        .transaction("words", "readwrite")
+        .objectStore("words")
+        .delete(index).result;
+    } catch (_) {
+      return; // TODO: error handling
+    }
   }
 
   function removeDB() {
