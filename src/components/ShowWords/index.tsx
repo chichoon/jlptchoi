b84766ebe144cc1ae5867c76722fd83fddf25d01
chichoon <chichoon.choi@gmail.com>
2023-07-08 00:42:01 +0900

    feat: :sparkles: 단어 및 발음 출력

--- src/components/ShowWords/index.tsx
+++ src/components/ShowWords/index.tsx
@@ -1,22 +1,58 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 
 import { useWordsDB } from "@/hooks/useWordsDB";
+import { Word } from "@/types/Words";
 
 export const ShowWords = () => {
-  const [count, setCount] = useState(0);
-  const { getWordCount } = useWordsDB();
+  const [count, setCount] = useState<number>(0);
+  const [currentWordKey, setCurrentWordKey] = useState<number>(0);
+  const [word, setWord] = useState<Word | null>(null);
+  const [errorMsg, setErrorMsg] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { getWord, getWordCount } = useWordsDB();
+
   useEffect(() => {
     getWordCount().then((value) => {
       setCount(value);
-      console.log(value);
     });
   }, []);
+
+  useEffect(() => {
+    setCurrentWordKey(Math.round(Math.random() * (count - 1)) + 1);
+  }, [count]);
+
+  useEffect(() => {
+    getWord(currentWordKey).then((value) => {
+      setWord(value);
+    });
+  }, [currentWordKey]);
+
+  useEffect(() => {
+    if (errorMsg.length === 0) return;
+    setTimeout(() => {
+      setErrorMsg("");
+    }, 1000);
+  }, [errorMsg]);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (inputRef.current === null || !word) return;
+    if (inputRef.current.value !== word.pronunciation) {
+      setErrorMsg("틀렸습니다!");
+      return;
+    }
+    setCurrentWordKey(Math.round(Math.random() * (count - 1)) + 1);
+    inputRef.current.value = "";
+  }
+
   return (
-    <div>
-      <h2>하이</h2>
-      <input type="text" autoFocus placeholder="발음을 입력" />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h2>{word?.word}</h2>
+      <input type="text" autoFocus placeholder="발음을 입력" ref={inputRef} />
+      <button type="submit">제출</button>
+      <span>{errorMsg}</span>
+    </form>
   );
 };
