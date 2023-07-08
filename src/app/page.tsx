65ec8472eb916f92147803eb8c5be00663eeab34
chichoon <chichoon.choi@gmail.com>
2023-07-08 20:09:25 +0900

    feat: :sparkles: 내비게이션 바 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import { ShowWords } from "@/components";
+import { NavBar } from "@/components/NavBar";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/import">
-        <span>단어 추가하러 가기</span>
-      </Link>
-      <ShowWords />
-    </div>
+    <>
+      <NavBar>
+        <>
+          <div className="flex-1" />
+          <Link href="/import" className="px-4 py-2">
+            <span>단어 추가</span>
+          </Link>
+          <Link href="/settings" className="inline-block flex-1 text-center">
+            설정
+          </Link>
+        </>
+      </NavBar>
+      <div className="flex-1 flex flex-col items-center">
+        <ShowWords />
+      </div>
+    </>
   );
 }
