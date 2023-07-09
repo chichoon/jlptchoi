ef564f0c259b297ee64e4f220b0200f79df26061
chichoon <chichoon.choi@gmail.com>
2023-07-09 21:26:03 +0900

    feat: :sparkles: 설정 페이지

--- src/app/import/page.tsx
+++ src/app/import/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ImportForm } from "@/components";
-import { NavBar, NavBarButton } from "@/components/NavBar";
+import { NavBar, NavBarButton, NavBarSpacer } from "@/components/NavBar";
 
 export default function Import() {
   return (
@@ -9,7 +9,8 @@ export default function Import() {
         <NavBarButton href="/">
           <span>홈</span>
         </NavBarButton>
-        <div className="flex-1" />
+        <NavBarSpacer />
+        <NavBarSpacer />
       </NavBar>
       <div>
         <h1>단어 추가</h1>
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import { Button, NavBar, NavBarButton, NavBarSpacer } from "@/components";
+import { useWordsDB } from "@/hooks";
+
 export default function Settings() {
-  return <div>설정</div>;
+  const { removeDB } = useWordsDB();
+  function handleReset() {
+    removeDB();
+  }
+  return (
+    <>
+      <NavBar>
+        <NavBarButton href="/">
+          <span>홈</span>
+        </NavBarButton>
+        <NavBarSpacer />
+        <NavBarSpacer />
+      </NavBar>
+      <div className="w-full flex flex-col p-2">
+        <h1 className="text-2xl w-full text-center mb-4">설정</h1>
+        <Button
+          type="button"
+          colorType="secondary"
+          text="초기화"
+          onClick={handleReset}
+        />
+      </div>
+    </>
+  );
 }
