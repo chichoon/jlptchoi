ba9a6be49cec7fb048e6abe6eac1f1ac573ea106
chichoon <chichoon.choi@gmail.com>
2023-07-10 12:54:10 +0900

    style: :lipstick: 모달 스타일 적용

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -20,13 +20,23 @@ interface ModalProps {
 export const Modal = ({ message, onClickClose, onClickOK }: ModalProps) => {
   return (
     <ModalPortal>
-      <div>
-        <div>
-          <span>{message}</span>
-        </div>
-        <div>
-          <button onClick={onClickClose}>취소</button>
-          <button onClick={onClickOK}>확인</button>
+      <div className="fixed flex items-center justify-center top-0 left-0 w-screen h-screen backdrop-blur-sm bg-white/30">
+        <div className="bg-white rounded-sm border-2">
+          <div className="p-2 border-b-2">
+            <span>{message}</span>
+          </div>
+          <div className="w-full flex flex-row justify-between items-center">
+            <button
+              onClick={onClickClose}
+              className="p-2 flex-1 hover:bg-slate-100 transition-colors">
+              취소
+            </button>
+            <button
+              onClick={onClickOK}
+              className="p-2 flex-1 hover:bg-slate-100 transition-colors">
+              확인
+            </button>
+          </div>
         </div>
       </div>
     </ModalPortal>
